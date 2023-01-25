b4c87791b706ce966bcfd0d4d479b32429a6c717
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-25 12:58:48 -0600

    rendered a list

--- src/App.js
+++ src/App.js
@@ -8,11 +8,35 @@ function MyButton() {
   )
 }
 
+const products = [
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
+];
+
+function ShoppingList() {
+  const listItems = products.map(product => 
+    <li
+      key={product.id}
+      style = {{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+    >
+      {product.title}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+
+}
+
 function App() {
   return (
     <div className="App">
       <h1>Welcome to my app</h1>
      <MyButton />
+     <ShoppingList />
     </div>
   );
 }
